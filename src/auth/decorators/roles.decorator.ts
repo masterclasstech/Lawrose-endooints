753d0ebe47541b7c2c24e597fc405b757cbde43d
omaks45 <danielgozie5@gmail..com>
2025-07-20 23:40:14 +0100

    role guard updated

---
+++ src/auth/decorators/roles.decorator.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+import { SetMetadata } from '@nestjs/common';
+import { UserRole } from '../../common/enums/user-role.enum';
+
+export const Roles = (...roles: UserRole[]) => SetMetadata('roles', roles);
