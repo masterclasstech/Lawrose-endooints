185d1451b54b467bd24c99be94654c0b141e41d5
omaks45 <danielgozie5@gmail..com>
2025-07-28 23:36:26 +0100

    product update dto

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
+/* eslint-disable prettier/prettier */
+import { PartialType, OmitType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(
+    OmitType(CreateProductDto, ['sku'] as const),
+) {}
